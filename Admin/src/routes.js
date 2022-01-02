49c844dcd696eda2b3a8a50df5f1575f40cb6e8c
TADAinGit <nguyenhuynhtatdat@gmail.com>
2022-01-03 01:24:24 +0700

    First commit

---
+++ Admin/src/routes.js
@@ -0,0 +1,46 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+//
+import Login from './pages/Login';
+import Register from './pages/Register';
+import DashboardApp from './pages/DashboardApp';
+import Products from './pages/Products';
+import Blog from './pages/Blog';
+import User from './pages/User';
+import Room from './pages/Room';
+import Maps from './pages/Maps';
+import NotFound from './pages/Page404';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  return useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" replace /> },
+        { path: 'app', element: <DashboardApp /> },
+        { path: 'user', element: <User /> },
+        { path: 'room', element: <Room /> },
+        { path: 'maps', element: <Maps /> },
+        { path: 'products', element: <Products /> },
+        { path: 'blog', element: <Blog /> }
+      ]
+    },
+    {
+      path: '/',
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: 'login', element: <Login /> },
+        { path: 'register', element: <Register /> },
+        { path: '404', element: <NotFound /> },
+        { path: '/', element: <Navigate to="/dashboard" /> },
+        { path: '*', element: <Navigate to="/404" /> }
+      ]
+    },
+    { path: '*', element: <Navigate to="/404" replace /> }
+  ]);
+}
